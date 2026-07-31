9296d55b9433afac6e5d9882997174a12cb4bfa0
Elliott Demain <edemain@wisc.edu>
2026-07-31 12:21:02 -0700

    fix empty requirements message

--- modules/descStats.R
+++ modules/descStats.R
@@ -302,9 +302,7 @@ descriptiveStatsServer <- function(id, shared, load_spectral_data_memo) {
       
       validate(
         need(isTRUE(shared$render_tick > 0),
-             "Confirm your model choices to see descriptive statistics."),
-        need(!is.null(shared$modelType) && nzchar(shared$modelType),
-             "Please select a stratification method to see descriptive statistics.")
+             "Confirm your model choices to see descriptive statistics.")
       )
       
       datatable(displayed_stats(), options = list(dom = 't', pageLength = -1), rownames = FALSE)
